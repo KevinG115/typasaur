7a74367e94ee59567d5c2a3e064d96276978b088
kevin gleeson <kevingleeson115@gmail.com>
2025-08-19 16:46:50 +0100

    Removing Metrics

--- app/src/core.js
+++ app/src/core.js
@@ -1,11 +1,5 @@
 const fs = require("fs");
 const path = require("path");
-// const telemetry = require("./telemetry.js");
-
-
-
-// Ask once per machine if telemetry is allowed
-
 
 /* ============================================================================
  * Entry
@@ -22,14 +16,6 @@ async function main() {
   const argvList = process.argv.slice(2);
   const args = parseCommandLineArguments(argvList);
   const isTty = process.stdin.isTTY;
-
-  // After you compute `args` from process.argv:
-  // args._rawArgv = process.argv.slice(2); // so telemetry can scrub flag names
-
-  // const telemetryAllowed = await telemetry.getOrEstablishTelemetryPreference(args);
-  // if (telemetryAllowed) {
-  //   telemetry.captureRunEvent(args);
-  // }
 
   printUsage(args);
   printBanner(args);
